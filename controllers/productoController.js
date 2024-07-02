7a8335ed0a9b98a050d23cde7b2a480dae7a31ee
AlanMain <alanncoradi@gmail.com>
2024-07-02 19:31:01 -0300

    Producto CRUD

--- controllers/productoController.js
+++ controllers/productoController.js
@@ -1,13 +1,54 @@
 const db = require('../db/db.js');
 
 const getAllProductos = (req,res) => {
-    const sql = "SELECT * FROM productos";
+    const sql = "SELECT * FROM producto";
     db.query(sql, (err,results) => {
         if (err) throw err;
         res.json(results);
-    })
+    });
+};
+
+const getProductoById = (req,res) => {
+    const {id} = req.params;
+    const sql = 'SELECT * FROM producto WHERE id=?';
+    db.query(sql,[id], (err, result) => {
+        if (err) throw err;
+        res.json(result);
+    });
+};
+
+const createProducto = (req,res) => {
+    const {nombre,descripcion,precio} = req.body;
+    const sql = 'INSERT INTO producto (nombre, descripcion, precio) VALUES (?, ?, ?)';
+    db.query(sql, [nombre, descripcion, precio], (err, result) => {
+        if (err) throw err;
+        res.json({ mensaje: "Producto creado correctamente", id_producto: result.insertId });
+    });
+};
+
+const updateProducto = (req, res) => {
+    const {id} = req.params;
+    const {nombre, descripcion, precio} = req.body;
+    const sql = 'UPDATE producto SET nombre = ?, descripcion = ?, precio = ? WHERE id = ?';
+    db.query(sql,[nombre,descripcion,precio,id], (err, result) => {
+        if (err) throw err;
+        res.json({ mensaje: "Producto actualizado correctamente"});
+    });
+};
+
+const deleteProducto = (req, res) => {
+    const {id} = req.params;
+    const sql = 'DELETE FROM producto WHERE id = ?';
+    db.query(sql,[id], (err, result) => {
+        if (err) throw err;
+        res.json({ mensaje: "Producto eliminado correctamente"});
+    });
 };
 
 module.exports = {
     getAllProductos,
+    getProductoById,
+    createProducto,
+    updateProducto,
+    deleteProducto
 };
--- routes/productoRoutes.js
+++ routes/productoRoutes.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const controller = require('../controllers/productoController.js');
 
 router.get('/', controller.getAllProductos);
+router.get('/:id', controller.getProductoById);
+router.post('/', controller.createProducto);
+router.put('/:id', controller.updateProducto);
+router.delete('/:id', controller.deleteProducto);
 
 module.exports = router;
